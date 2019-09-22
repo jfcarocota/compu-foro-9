ed0c9187cf7e10a5c2cb919319fe066de20fa87f
jfcarocota <jfcarocota@gmail.com>
2019-09-22 15:02:37 -0700

    quicksort with dutch national flag solved

--- src/Quicksort.hh
+++ src/Quicksort.hh
@@ -1,16 +1,60 @@
 class Quicksort
 {
 private:
-    /* data */
+    const static void Partition(int[], int, int, int&, int&); 
 public:
+    const static void Sorting(int[], int, int);
     Quicksort(/* args */);
     ~Quicksort();
 };
 
-Quicksort::Quicksort(/* args */)
+Quicksort::Quicksort()
 {
 }
 
 Quicksort::~Quicksort()
 {
 }
+
+
+//Dutch National Flag Algorithm 
+const void Quicksort::Partition(int a[], int low, int high, int &i, int &j) 
+{ 
+    if (high - low <= 1) 
+    { 
+        if (a[high] < a[low]) 
+            std::swap(a[high], a[low]); 
+        i = low; 
+        j = high; 
+        return; 
+    } 
+  
+    int mid = low; 
+    int pivot = a[high]; 
+    while (mid <= high) 
+    { 
+        if (a[mid]<pivot) 
+            std::swap(a[low++], a[mid++]); 
+        else if (a[mid]==pivot) 
+            mid++; 
+        else if (a[mid]>pivot) 
+            std::swap(a[mid], a[high--]); 
+    } 
+  
+    //update i and j 
+    i = low-1; 
+    j = mid; //or high-1 
+} 
+  
+const void Quicksort::Sorting(int a[], int low, int high) 
+{ 
+    if (low>=high) 
+        return; 
+  
+    int i, j; 
+  
+    Quicksort::Partition(a, low, high, i, j); 
+  
+    Quicksort::Sorting(a, low, i); 
+    Quicksort::Sorting(a, j, high); 
+} 
--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,20 @@
 #include<iostream>
 #include "Quicksort.hh"
 
-int main()
-{
-    std::cout << "Hello world" << std::endl;
+  
+int main() 
+{ 
+    int a[] = {4, 9, 4, 4, 1, 9, 4, 4, 9, 4, 4, 1, 4}; 
+
+    int size = sizeof(a) / sizeof(int); 
+  
+    Quicksort::Sorting(a, 0, size - 1); 
+
+    for (int i = 0; i < size; ++i) 
+    {
+        std::cout << a[i] << " ";
+    }
+
     std::getchar();
-    return 0;
-}
+    return 0; 
+} 
